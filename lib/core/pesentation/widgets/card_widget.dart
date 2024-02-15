d6505da2acf68e12208f42c9015824b9de8ad843
JhonKing4 <jhoandiciaucetz@gmail.com>
2024-02-14 20:33:23 -0500

    Banco

---
+++ lib/core/pesentation/widgets/card_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class CardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: 3.0,
+          margin: EdgeInsets.all(1.0),
+          color: (Colors.white),
+        ),
+        Image.asset("assets/images/iconbanorte.png", height: 40.0),
+        const Text(
+          "Banorte",
+          style: TextStyle(
+              color: Colors.white, fontSize: 38.0, fontFamily: "Card"),
+        ),
+        const Text(
+          "Debito",
+          style: TextStyle(color: Colors.white, fontSize: 15.0),
+        ),
+      ],
+    );
+  }
+}
+
+class CardWidget1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          height: 1.0,
+          margin: EdgeInsets.all(1.0),
+          color: (Colors.white),
+        ),
+        const Text(
+          "04/30",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        Image.asset(
+          "assets/images/chip.png",
+          height: 50.0,
+        )
+      ],
+    );
+  }
+}
+
+class CardWidget2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: 1.0,
+          margin: EdgeInsets.all(1.0),
+          color: (Colors.white),
+        ),
+        const Text(
+          "Jhoandi Abraham Ciau Cetz",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        Image.asset("assets/images/visa.png", height: 30.0),
+      ],
+    );
+  }
+}
+
