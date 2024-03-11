184a6b8791c0ebc6f42cf0c85617efbf2a3c22fe
JhonKing4 <jhoandiciaucetz@gmail.com>
2024-03-11 17:42:27 -0500

    kotlin

--- android/app/src/main/kotlin/com/example/banking/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/banking/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.banking
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.pluggins.GeneratedPluginsRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginsRegistrant.registerWith(flutterEngine)
+    }
 }
